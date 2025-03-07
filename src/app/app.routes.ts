5a99e4a2cd9786604ca926bf5a8be3a0019a167d
Atharva <atharvasharma.cs25@gmail.com>
2025-03-07 13:16:08 +0530

    Designed add contact form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes , RouterModule} from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AddContactComponent } from './pages/add-contact/add-contact.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'add-contact', component: AddContactComponent } 
 ];
